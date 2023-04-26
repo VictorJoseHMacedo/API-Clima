a764b4d76cf3b6454c3e1cbf51c8e7aed8cee216
Victor José <112560183+VictorJoseHMacedo@users.noreply.github.com>
2023-04-26 12:46:33 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
 // Variaveis e Seleção de Elementos
-const apiKey = "ee8b8017fc4f9e4c136c96ad89ebb1aa"; // numero da API
-const apiCountryURL = ""; // Api das bandeiras dos paises
-const apiUnsplash = "https://source.unsplash.com/1600x900/?"; //API das fotos que vão ao background
 
-const cityInput = document.querySelector("#city-input"); // Pegando id da cidade
-const searchBtn = document.querySelector("#search"); // Pegando id do search
+/** Pegando id de todos os itens da página */
+const apiKey = ""; 
+const apiCountryURL = ""; 
+const apiUnsplash = "https://source.unsplash.com/1600x900/?"; 
 
-const cityElement = document.querySelector("#city"); // Pegando id da cidade
-const tempElement = document.querySelector("#temperature span"); // Pegando id da temperatura
-const descElement = document.querySelector("#description"); // Pegando id da descrição
-const weatherIconElement = document.querySelector("#weather-icon"); // Pegando id do icone da temperatura
-const countryElement = document.querySelector("#country"); // Pegando id da bandeira do pais
-const umidityElement = document.querySelector("#umidity span"); // Pegando id da umidade
-const windElement = document.querySelector("#wind span"); // Pegando id do vento
+const cityInput = document.querySelector("#city-input"); 
+const searchBtn = document.querySelector("#search"); 
+
+const cityElement = document.querySelector("#city"); 
+const tempElement = document.querySelector("#temperature span"); 
+const descElement = document.querySelector("#description"); 
+const weatherIconElement = document.querySelector("#weather-icon"); 
+const countryElement = document.querySelector("#country"); 
+const umidityElement = document.querySelector("#umidity span"); 
+const windElement = document.querySelector("#wind span"); 
 
 const weatherContainer = document.querySelector("#weather-data");
 
-const errorMessageContainer = document.querySelector("#error-message"); // Pegando id da mensagem de erro
-const loader = document.querySelector("#loader"); // Pegando id do botão de carregar
+const errorMessageContainer = document.querySelector("#error-message"); 
+const loader = document.querySelector("#loader"); 
 
-const suggestionContainer = document.querySelector("#suggestions"); // Pegando id que sugere paises
-const suggestionButtons = document.querySelectorAll("#suggestions button"); // Pegando id dos botões que sugere paises
+const suggestionContainer = document.querySelector("#suggestions"); 
+const suggestionButtons = document.querySelectorAll("#suggestions button"); 
 
 // Loader
-const toggleLoader = () => { // Botão de carregar
+
+/** Botão de Carregar */
+const toggleLoader = () => { 
   loader.classList.toggle("hide");
 };
 
@@ -45,11 +49,11 @@ const showErrorMessage = () => {
   errorMessageContainer.classList.remove("hide");
 };
 
-const hideInformation = () => { // Essa função é responsável por ocultar informações anteriores na interface do usuário relacionadas aos dados do clima
-  errorMessageContainer.classList.add("hide"); // Ocultará ou esconderá visualmente o elemento na interface do usuário.
-  weatherContainer.classList.add("hide"); // Ocultará ou esconderá visualmente o elemento na interface do usuário.
+const hideInformation = () => { 
+  errorMessageContainer.classList.add("hide"); 
+  weatherContainer.classList.add("hide"); 
 
-  suggestionContainer.classList.add("hide"); // Ocultará ou esconderá visualmente o elemento na interface do usuário.
+  suggestionContainer.classList.add("hide");
 };
 
 const showWeatherData = async (city) => { //Exibir os dados do clima para uma determinada cidade
@@ -57,29 +61,31 @@ const showWeatherData = async (city) => { //Exibir os dados do clima para uma de
 
   const data = await getWeatherData(city);
 
-  if (data.cod === "404") { // Usado para verificar se a resposta da API do clima indicou que a cidade especificada não foi encontrada
+  /** Mensagem erro */
+  if (data.cod === "404") { 
     showErrorMessage();
     return;
   }
 
-  cityElement.innerText = data.name; // Nome da cidade
-  tempElement.innerText = parseInt(data.main.temp); // Temperatura da cidade
-  descElement.innerText = data.weather[0].description; // Texto da temperatura
-  weatherIconElement.setAttribute( // Icone da temperatura
+  /** Pegando os dados na API */
+  cityElement.innerText = data.name; 
+  tempElement.innerText = parseInt(data.main.temp); 
+  descElement.innerText = data.weather[0].description; 
+  weatherIconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
   );
-  countryElement.setAttribute("src", apiCountryURL + data.sys.country); // Pegando a foto do pais
-  umidityElement.innerText = `${data.main.humidity}%`; // Pegando a humidade
-  windElement.innerText = `${data.wind.speed}km/h`; // Pegando a velocidade do vento
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country); 
+  umidityElement.innerText = `${data.main.humidity}%`; 
+  windElement.innerText = `${data.wind.speed}km/h`; 
 
-  // Change bg image
-  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`; // Alterar a imagem do background 
+  /** Alterando imagem do background */
+  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
 
   weatherContainer.classList.remove("hide");
 };
 
-searchBtn.addEventListener("click", async (e) => { // Quando clicar no botão de pesquisar, aparecer na tela
+searchBtn.addEventListener("click", async (e) => { 
   e.preventDefault();
 
   const city = cityInput.value;
@@ -87,7 +93,8 @@ searchBtn.addEventListener("click", async (e) => { // Quando clicar no botão de
   showWeatherData(city);
 });
 
-cityInput.addEventListener("keyup", (e) => { // Adicionando pequisar com botão enter do teclado
+/** Pesquisa pressionando Enter */
+cityInput.addEventListener("keyup", (e) => { 
   if (e.code === "Enter") {
     const city = e.target.value; 
 
@@ -95,8 +102,9 @@ cityInput.addEventListener("keyup", (e) => { // Adicionando pequisar com botão 
   }
 });
 
-// Sugestões
-suggestionButtons.forEach((btn) => { // Quando clicar no botão das sugestões de clima, aparecer na tela
+
+ /** Paises recomendados */
+suggestionButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     const city = btn.getAttribute("id");
 
